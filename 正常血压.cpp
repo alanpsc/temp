055a97be589cd7a1c054805c700c765a37ae4c7b
WUCANLU <632850375@qq.com>
2016-02-18 10:41:46 +0800

    Update 正常血压.cpp

--- 正常血压.cpp
+++ 正常血压.cpp
@@ -1,13 +1,14 @@
-#include <cstdio>;
+#include <cstdio>
 int main() 
 {
-    int n,D,S,sum;
-    scanf("%d\n",&n);
+    int n,D,S,sum=0;
+    scanf("%d",&n);
 	for(int i=1;i<=n;++i){
-	   scanf("%d %d\n",&D,&S);
+	   scanf("%d %d",&D,&S);
             if((90<=S<=140)&&(60<=D<=90)){
                sum+=1;
-			}
+	    }
+	    
     }
     printf("%d",sum);
      return 0;
