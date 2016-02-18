d7112078ea40de296719575c5caa950b39179073
WUCANLU <632850375@qq.com>
2016-02-18 11:17:14 +0800

    Update 求一元二次方程的根.cpp
    目前问题是  sqrt后的数据无法计算并输出
    
    利用公式x1 = (-b + sqrt(b*b-4*a*c))/(2*a), x2 = (-b - sqrt(b*b-4*a*c))/(2*a)求一元二次方程ax2+ bx + c =0的根，其中a不等于0。
    
    输入
    输入一行，包含三个浮点数a, b, c（它们之间以一个空格分开），分别表示方程ax2 + bx + c =0的系数。
    输出
    输出一行，表示方程的解。
    若b2 = 4 * a * c,则两个实根相等，则输出形式为：x1=x2=...。
    若b2 > 4 * a * c,则两个实根不等，则输出形式为：x1=...;x2 = ...，其中x1>x2。
    若b2 < 4 * a * c，则有两个虚根，则输出：x1=实部+虚部i; x2=实部-虚部i，即x1的虚部系数大于等于x2的虚部系数，实部为0时不可省略。实部 = -b / (2*a), 虚部 = sqrt(4*a*c-b*b) / (2*a)
    
    所有实数部分要求精确到小数点后5位，数字、符号之间没有空格。
    样例输入
    样例输入1
    1.0 2.0 8.0
    
    样例输入2
    1 0 1
    样例输出
    样例输出1
    x1=-1.00000+2.64575i;x2=-1.00000-2.64575i
    
    样例输出2
    x1=0.00000+1.00000i;x2=0.00000-1.00000i

--- 求一元二次方程的根.cpp
+++ 求一元二次方程的根.cpp
@@ -1,15 +1,22 @@
-#include <cstdio>;
+#include <cstdio>
+#include <cmath>
 int main() 
 {
-    float x1,x2,a,b,c;
-	scanf("%f %f %f",&a,&b,&c);
-     x1=(-b+sqrt(b*b-4*a*c))/(2*a);
-	 x2=(-b+sqrt(b*b-4*a*c))/(2*a);
-	 if(b*b==4*a*c)
-		 printf("x1=x2=%.5f",x1);
-	 else if(b*b>4*a*c)
-		 printf("x1=%.5f,x2=%.5f",x1,x2);
-	 else 
-		 printf("x1=-b/(2*a)+sqrt(4*a*c-b*b)/(2*a)i,x2=-b/(2*a)-sqrt(4*a*c-b*b)/(2*a)i");
+    float x1, x2, x3, x4, a, b, c;
+    char i;
+	scanf("%f %f %f", &a, &b, &c);
+     x1 = (-b+sqrt(b*b-4*a*c))/(2*a);
+	 x2 = (-b-sqrt(b*b-4*a*c))/(2*a);
+	 if(b*b==4*a*c){
+		 printf("x1 = x2= %.5f", x1);
+	}
+	 else if(b*b > 4*a*c){
+		 printf("x1 = %.5f,x2 = %.5f", x1, x2);
+	}
+	 else if(b*b < 4*a*c){
+	 	x3 = -b/(2*a)+sqrt(4*a*c-b*b)/(2*a)*i;
+	 	x4 = -b/(2*a)-sqrt(4*a*c-b*b)/(2*a)*i;
+	}
+		printf("x1=%.5fi,x2=%.5fi",x3,x4);
      return 0;
 }
